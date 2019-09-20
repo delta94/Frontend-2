ec6f749abcd3f3eafbab1afe587d41dbfa54b02a
PhuongNV <phuongnv@toprate.io>
2019-09-20 10:14:40 +0700

    fixbug

--- src/main/webapp/app/views/pages/landing/landing.tsx
+++ src/main/webapp/app/views/pages/landing/landing.tsx
@@ -40,117 +40,90 @@ export class Landing extends React.Component<ILandingProps, ILandingState> {
     const script = document.createElement('script');
 
     script.innerHTML = this.props.script;
+
     script.innerHTML = `
-    
-  
-	
-    function onSubmit() {
-      toastr.options = {
-        "closeButton": true,
-        "debug": false,
-        "newestOnTop": false,
-        "progressBar": false,
-        "positionClass": "toast-bottom-right",
-        "preventDuplicates": false,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-      }
-
-      var path = window.location.href;
-      var arr = path.split("/");
-      var campaign = arr[arr.indexOf("mgm")+ 1];
-      var customer = arr[arr.indexOf("customer")+ 1];
-	  
-	  
-      var form=document.getElementById('mgm');
-      var campaignEl =  document.getElementById('campaign');
-      if (!campaignEl){
-            campaignEl = document.createElement('input');//prepare a new input DOM element
-			campaignEl.setAttribute('id', "campaign");//set the param name
-            campaignEl.setAttribute('name', "campaign");//set the param name
-			campaignEl.setAttribute('value', campaign);//set the value
-			campaignEl.setAttribute('type', 'hidden')//set the type
-			form.appendChild(campaignEl);
-		}
-		var customerEl =  document.getElementById('customer');
-		if (!customerEl){
-		customerEl = document.createElement('input');//prepare a new input DOM element
-		customerEl.setAttribute('id', "customer");//set the param name
-		customerEl.setAttribute('name', "customer");//set the param name
-        customerEl.setAttribute('value', customer);//set the value
-        customerEl.setAttribute('type', 'hidden')//set the type
-      form.appendChild(customerEl);
-      }
-
-      var successToRq = true;
-      var valueName = document.getElementsByName('name')[0].value;
-      var valueEmail = document.getElementsByName('email')[0].value;
-      var valuePhone = document.getElementsByName('phone')[0].value;
-	  
-      var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}/igm;
-      var vnfont = /((09|03|07|08|05)+([0-9]{8})\b)/g;
-	  
-	  document.getElementsByName('errorName')[0].style.display = 'none';
-	  document.getElementsByName('errorEmail')[0].style.display = 'none';
-	  document.getElementsByName('errorPhone')[0].style.display = 'none';
-	  
-	  if(valueName==="" || valueName===undefined){
-		document.getElementsByName('errorName')[0].style.display = 'block';
-		document.getElementsByName('errorName')[0].innerHTML="&nbsp;* Tên bắt buộc nhập"; 
-		document.getElementsByName('errorName')[0].style.color = 'red';
-		successToRq = false;
-	  } if(valueEmail==="" || valueEmail===undefined){
-		document.getElementsByName('errorEmail')[0].style.display = 'block';
-		document.getElementsByName('errorEmail')[0].innerHTML="&nbsp;* Email bắt buộc nhập"; 
-		document.getElementsByName('errorEmail')[0].style.color = 'red';
-		successToRq = false;
-	  }	else if(!re.test(valueEmail)){
-		document.getElementsByName('errorEmail')[0].style.display = 'block';
-		document.getElementsByName('errorEmail')[0].innerHTML="&nbsp;* Email không hợp lệ"; 
-		document.getElementsByName('errorEmail')[0].style.color = 'red';
-		successToRq = false;
-	  } 
-	  if(valuePhone==="" || valuePhone===undefined){
-		document.getElementsByName('errorPhone')[0].style.display = 'block';
-		document.getElementsByName('errorPhone')[0].innerHTML="&nbsp;* Số điện thoại bắt buộc nhập"; 
-		document.getElementsByName('errorPhone')[0].style.color = 'red';
-		successToRq = false;
-	  } else if(!vnfont.test(valuePhone)){
-		 document.getElementsByName('errorPhone')[0].style.display = 'block';
-		 document.getElementsByName('errorPhone')[0].innerHTML="&nbsp;* Số điện thoại không hợp lệ"; 
-		 document.getElementsByName('errorPhone')[0].style.color = 'red';
-		 successToRq = false;
-	  }
-	    
-      if(successToRq) {
-        var xhttp = new XMLHttpRequest();
-              xhttp.onreadystatechange = function() {
-				if(this.readyState == 4){
-					if(this.status == 400){	
-						var obj = JSON.parse(this.responseText);
-						toastr.error(obj.message)
-					}else if(this.status == 200){
-						toastr.success(this.responseText);
-					}
-				}
-            };
-            xhttp.open("POST", 'http://171.244.40.91:8088/v1/campaign/'+ campaign+'/customer/'+customer, true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            var postString = 'campaign='+document.getElementsByName('campaign')[0].value + '&customer='+document.getElementsByName('customer')[0].value
-                      + '&name='+document.getElementsByName('name')[0].value
-                      + '&email='+document.getElementsByName('email')[0].value
-                      + '&phone='+document.getElementsByName('phone')[0].value
-            xhttp.send(postString);
-              }
-      form.submit();
+    toastr.options = {
+      "closeButton": true,
+      "debug": false,
+      "newestOnTop": false,
+      "progressBar": false,
+      "positionClass": "toast-bottom-right",
+      "preventDuplicates": false,
+      "onclick": null,
+      "showDuration": "300",
+      "hideDuration": "1000",
+      "timeOut": "5000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
     }
+
+
+    function showErrorElement(elementName,errorValue) {
+      document.getElementsByName(elementName)[0].style.display = 'block';
+      document.getElementsByName(elementName)[0].innerHTML= errorValue; 
+      document.getElementsByName(elementName)[0].style.color = 'red';
+    }
+    
+      function onSubmit() {
+      
+        var path = window.location.href;
+        var arr = path.split("/");
+        var campaign = arr[arr.indexOf("mgm")+ 1];
+        var customer = arr[arr.indexOf("customer")+ 1];
+      
+        var successToRq = true;
+        var valueName = document.getElementsByName('name')[0].value;
+        var valueEmail = document.getElementsByName('email')[0].value;
+        var valuePhone = document.getElementsByName('phone')[0].value;
+      
+        var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}/igm;
+        var vnfont = /((09|03|07|08|05)+([0-9]{8})$)/g;
+      
+      document.getElementsByName('errorName')[0].style.display = 'none';
+      document.getElementsByName('errorEmail')[0].style.display = 'none';
+      document.getElementsByName('errorPhone')[0].style.display = 'none';
+      
+      if(valueName==="" || valueName===undefined){
+      showErrorElement('errorName',"&nbsp;* Tên bắt buộc nhập");
+      successToRq = false;
+      } 
+      if(valueEmail==="" || valueEmail===undefined){
+      showErrorElement('errorEmail',"&nbsp;* Email bắt buộc nhập");
+      successToRq = false;
+      }	else if(!re.test(valueEmail)){
+      showErrorElement('errorEmail',"&nbsp;&nbsp; Email không hợp lệ");
+      successToRq = false;
+      } 
+      if(valuePhone==="" || valuePhone===undefined){
+      showErrorElement('errorPhone',"&nbsp;* Số điện thoại bắt buộc nhập");
+      successToRq = false;
+      } else if(!vnfont.test(valuePhone)){
+       showErrorElement('errorPhone',"&nbsp;&nbsp; Số điện thoại không hợp lệ");
+       successToRq = false;
+      }
+        if(successToRq) {
+          var xhttp = new XMLHttpRequest();      
+              xhttp.open("POST", 'http://171.244.40.91:8088/v1/campaign/'+ campaign+'/customer/'+customer, true);
+              xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+              var postString = 'campaign=' + campaign + '&customer=' + customer + '&name=' + valueName
+                        + '&email=' + valueEmail+ '&phone=' + valuePhone
+              
+        xhttp.onreadystatechange = function() {
+          if(this.readyState == 4){
+            if(this.status == 400){	
+              var obj = JSON.parse(this.responseText);
+              toastr.error(obj.message)
+            } else if(this.status == 200){
+              toastr.success(this.responseText);
+            }
+          }
+              };		  
+              xhttp.send(postString);
+          }
+      }
   `;
     script.async = true;
 
