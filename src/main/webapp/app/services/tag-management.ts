30f9863a265ea4e53c5c161bf341b43cbfe2154d
trancongtuan525 <trancongtuan525@gmail.com>
2019-10-29 17:54:32 +0700

    update code

--- src/main/webapp/app/services/tag-management.ts
+++ src/main/webapp/app/services/tag-management.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const tagUrl = 'v1/tags';
 
-export const getListTags = () => {
+export const getListTags = (textSearch, page, pageSize) => {
   // return axios.get('http://localhost:9000/content/json_data/tag_data.json');
   return axios.get(tagUrl);
 };
