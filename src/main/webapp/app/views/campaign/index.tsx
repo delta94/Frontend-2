43449a4a6007fb0fc6bfbc5f7d211bc3be607375
trancongtuan525 <trancongtuan525@gmail.com>
2019-11-29 09:59:21 +0700

    updata authen -tuantc

--- src/main/webapp/app/views/campaign/index.tsx
+++ src/main/webapp/app/views/campaign/index.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import ErrorBoundaryRoute from 'app/common/error/error-boundary-route';
+import PrivateRoute from '../../common/auth/private-route';
 import CampaginManagament from './campaign-automation/campaign-managament/campaign-managament';
 import UserCampaign from './user-campaign';
 
@@ -7,6 +8,9 @@ const Routes = ({ match }) => (
   <Fragment>
     <ErrorBoundaryRoute path={`${match.url}`} exact={true} component={CampaginManagament} />
     <ErrorBoundaryRoute path={`app/views/campaigns/user-campaign`} exact={true} component={UserCampaign} />
+
+    <PrivateRoute path={`${match.url}`} exact={true} component={CampaginManagament} />
+    <PrivateRoute path={`app/views/campaigns/user-campaign`} exact={true} component={UserCampaign} />
   </Fragment>
 );
 
