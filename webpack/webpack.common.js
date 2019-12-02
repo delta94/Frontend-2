f514ec8bc7d575641f76d511ac90c30b4418ce75
trancongtuan525 <trancongtuan525@gmail.com>
2019-12-02 11:19:49 +0700

    update -tuantc

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -109,8 +109,8 @@ module.exports = options => ({
         //er, in `prod` mode, you will need to enable CORS
         // (see the `jhipster.cors` common JHipster property in the `application-*.yml` configurations)
         // SERVER_API_URL: options.env === 'development' ? `'http://interview-test.topica.vn:8080/'` : `'http://interview.topica.vn:8080/'`
-        SERVER_API_URL: `'http://127.0.0.1:8088/'`
-        // SERVER_API_URL: `'http://171.244.40.91:8088/'`
+        // SERVER_API_URL: `'http://127.0.0.1:8088/'`
+        SERVER_API_URL: `'http://171.244.40.91:8088/'`
         // SERVER_API_URL: options.env === 'development' ? `'http://5d521fdf3432e70014e6b5ae.mockapi.io/'` : `'http://5d521fdf3432e70014e6b5ae.mockapi.io/'`
         // SERVER_API_URL: `'http://192.168.1.63:8088/'`
         // SERVER_API_URL: `''`
