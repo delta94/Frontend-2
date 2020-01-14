490dbe117d18be8653d001d4f1d4e730b3a38876
tuan.tran@gosu.vn <tuan.tran@gosu.vn>
2020-01-14 11:48:25 +0700

    update preview -tuantc

--- src/main/webapp/app/views/GGeditor/config-email/preview/preview.tsx
+++ src/main/webapp/app/views/GGeditor/config-email/preview/preview.tsx
@@ -2,9 +2,11 @@ import React, { CSSProperties } from 'react';
 import DOMPurify from 'dompurify';
 import './preview.scss';
 import { connect } from 'react-redux';
+import { Col, Row, Button, Container } from 'reactstrap';
 import { IRootState } from 'app/reducers';
+import { isThisSecond } from 'date-fns';
 
-export interface IPreviewLandingProps extends StateProps, DispatchProps {}
+export interface IPreviewLandingProps extends StateProps, DispatchProps { }
 
 export interface IPreviewLandingProps {
   htmlDOM?: string;
@@ -14,8 +16,9 @@ export interface IPreviewLandingProps {
 
 interface IPreviewLandingState {
   htmlDOM?: string;
-  styleDOM?: string;
+  styleForDOM?: string;
   scriptDOM?: string;
+  showMobile: boolean
 }
 
 class PreviewLanding extends React.PureComponent<IPreviewLandingProps, IPreviewLandingState> {
@@ -23,13 +26,14 @@ class PreviewLanding extends React.PureComponent<IPreviewLandingProps, IPreviewL
     super(props);
   }
 
-  state = {
+  state: IPreviewLandingState = {
     htmlDOM: '',
     styleForDOM: '',
-    scriptDOM: 'function test(){alert("test")}'
+    scriptDOM: 'function test(){alert("test")}',
+    showMobile: false
   };
 
-  setValueForPopUp = event => {};
+  setValueForPopUp = event => { };
 
   escapeRegExp = string => {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
@@ -60,7 +64,25 @@ class PreviewLanding extends React.PureComponent<IPreviewLandingProps, IPreviewL
   }
 
   render() {
-    return <iframe id="fred" width="100%" height="500px" />;
+    let { showMobile } = this.state
+    return (
+      <Container>
+        <Row>
+          <Col sm="12" md={{ size: 6, offset: 3 }} style={{ textAlign: "center" }}>
+            <Button color="none" style={{ fontSize: "24px" }} onClick={() => this.setState({ showMobile: false })}>
+              <i className="pe-7s-monitor"> </i></Button>
+            <Button color="none" style={{ fontSize: "24px" }} onClick={() => this.setState({ showMobile: true})}>
+              <i className="pe-7s-phone"> </i></Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={showMobile ? "12" : ""} md={showMobile ? { size: 6, offset: 3 } : ''}>
+            <iframe id="fred" width="100%" height="500px" />
+          </Col>
+        </Row>
+      </Container>
+    )
+
   }
 }
 
