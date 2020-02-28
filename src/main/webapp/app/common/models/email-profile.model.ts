b5123f424cf51f4b363ebf8703554ffa8617e756
Luan Nguyen <luannv@intelads.vn>
2020-02-28 12:53:56 +0700

    fix bugs and add email-profile

---
+++ src/main/webapp/app/common/models/email-profile.model.ts
@@ -0,0 +1,36 @@
+export interface EmailProfile {
+  id: string;
+  email: string;
+  fromName: string;
+  isDefault: number;
+  isActivated: number;
+}
+
+export interface IEmailProfileData {
+  content: [EmailProfile];
+  empty: boolean;
+  first: boolean;
+  last: boolean;
+  number: number;
+  numberOfElements: number;
+  pageable: {
+    offset: number;
+    pageNumber: number;
+    pageSize: number;
+    paged: boolean;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    unpaged: boolean;
+  };
+  size: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  totalElements: number;
+  totalPages: number;
+}
